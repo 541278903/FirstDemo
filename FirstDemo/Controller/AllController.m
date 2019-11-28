2db0440550b9649d8f5255bc4cc05cf7059f9687
a541278903 <534272374@qq.com>
2019-11-28 14:56:33 +0800

    last

--- FirstDemo/Controller/AllController.m
+++ FirstDemo/Controller/AllController.m
@@ -7,8 +7,19 @@
 //
 
 #import "AllController.h"
+#import "ViewController.h"
 
 @implementation AllController
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.ConName = @"未初始化";
+        self.ConView = [[ViewController alloc]init];
+        self.ConView.title = self.ConName;
+    }
+    return self;
+}
 - (instancetype)initWithName:(NSString *)name  Con:(UIViewController *)con
 {
     self = [super init];
@@ -18,6 +29,9 @@
         self.ConView.title = self.ConName;
     }
     return self;
+}
+-(BOOL)isNull{
+    return self.ConView == NULL;
 }
 
 @end
