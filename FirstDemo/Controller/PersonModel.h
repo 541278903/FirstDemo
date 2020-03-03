3fdd8f1a90cef66ba22e0d81beada62793b27053
a541278903 <534272374@qq.com>
2020-03-03 10:52:12 +0800

    last

---
+++ FirstDemo/Controller/PersonModel.h
@@ -0,0 +1,17 @@
+//
+//  PersonModel.h
+//  FirstDemo
+//
+//  Created by edward on 2020/3/3.
+//  Copyright © 2020 com.Edward. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PersonModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FirstDemo/Controller/PersonModel.m
@@ -0,0 +1,17 @@
+//
+//  PersonModel.m
+//  FirstDemo
+//
+//  Created by edward on 2020/3/3.
+//  Copyright © 2020 com.Edward. All rights reserved.
+//
+
+@interface PersonModel()
+
+
+@end
+
+
+@implementation PersonModel
+
+@end
